72cd4d30413a18fc3d8170b785738977c76b8fb0
Amine ABOUTAJ <amine.aboutaj@atos.net>
2023-03-21 16:12:19 +0000

    end keycloack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SecurityService} from './services/security.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'BankManagementFront';
+export class AppComponent implements OnInit{
+ title = 'BankManagementFront';
+ constructor(public securityService:SecurityService) { }
+
+ public async ngOnInit() {
+
+ }
+
+ onLogout() {
+   this.securityService.kcService.logout(window.location.origin);
+ }
+ public async getToken() {
+ }
+
+ async login(){
+   await this.securityService.kcService.login({
+     redirectUri : window.location.origin
+   })
+ }
 }
